56d55b6906593ff4aa927b4defb013ac6d0ea857
ArinoLane <arindom793@gmail.com>
2025-06-09 00:08:00 +0600

    commiting to my brunch

--- VehicleServiceCenter/Form2.Designer.cs
+++ VehicleServiceCenter/Form2.Designer.cs
@@ -28,90 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Panel();
             tableLayoutPanel1 = new TableLayoutPanel();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            panel1.SuspendLayout();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
-            // 
-            // panel1
-            // 
-            panel1.AutoScroll = true;
-            panel1.AutoScrollMargin = new Size(800, 500);
-            panel1.AutoScrollMinSize = new Size(0, 800);
-            panel1.Controls.Add(button5);
-            panel1.Controls.Add(button4);
-            panel1.Controls.Add(button3);
-            panel1.Controls.Add(button2);
-            panel1.Controls.Add(button1);
-            panel1.Location = new Point(0, 55);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(800, 395);
-            panel1.TabIndex = 0;
             // 
             // tableLayoutPanel1
             // 
-            tableLayoutPanel1.BackColor = Color.FromArgb(31, 31, 31);
-            tableLayoutPanel1.ColumnCount = 2;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.Dock = DockStyle.Top;
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel1.Controls.Add(panel2, 0, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 2;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 49F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 51F));
-            tableLayoutPanel1.Size = new Size(800, 49);
-            tableLayoutPanel1.TabIndex = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 11.333333F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 88.6666641F));
+            tableLayoutPanel1.Size = new Size(800, 450);
+            tableLayoutPanel1.TabIndex = 0;
             // 
-            // button1
+            // panel1
             // 
-            button1.Location = new Point(347, 281);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(794, 45);
+            panel1.TabIndex = 0;
             // 
-            // button2
+            // panel2
             // 
-            button2.Location = new Point(347, 490);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 0;
-            button2.Text = "button1";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(347, 661);
-            button3.Name = "button3";
-            button3.Size = new Size(75, 23);
-            button3.TabIndex = 0;
-            button3.Text = "button1";
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            button4.Location = new Point(347, 862);
-            button4.Name = "button4";
-            button4.Size = new Size(75, 23);
-            button4.TabIndex = 0;
-            button4.Text = "button1";
-            button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            button5.Location = new Point(347, 1080);
-            button5.Name = "button5";
-            button5.Size = new Size(75, 23);
-            button5.TabIndex = 0;
-            button5.Text = "button1";
-            button5.UseVisualStyleBackColor = true;
+            panel2.AutoScroll = true;
+            panel2.AutoScrollMinSize = new Size(0, 1000);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(3, 54);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(794, 393);
+            panel2.TabIndex = 1;
             // 
             // Form2
             // 
@@ -120,21 +75,17 @@
             BackColor = Color.FromArgb(18, 18, 18);
             ClientSize = new Size(800, 450);
             Controls.Add(tableLayoutPanel1);
-            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "Form2";
             Text = "Form2";
-            panel1.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Panel panel1;
         private TableLayoutPanel tableLayoutPanel1;
-        private Button button5;
-        private Button button4;
-        private Button button3;
-        private Button button2;
-        private Button button1;
+        private Panel panel1;
+        private Panel panel2;
     }
 }
