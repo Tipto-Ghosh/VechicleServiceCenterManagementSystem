c40c1c96c735b33a99ee630dc3edf65652ada7a1
ReDThunDeR33 <mdarko190@gmail.com>
2025-06-09 02:35:37 +0600

    aa

---
+++ VehicleServiceCenter/Config/Config/DbConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace VehicleServiceCenter.Config {
+
+    // This is a class where we do the database connection part
+    public static class DbConfig {
+        // Update the connection string 
+        public static string connectionString =
+           @"Data Source=TECH\SQLEXPRESS;Integrated Security=True;Trust Server Certificate=True";
+
+        // This a method which will create a SqlConnection and return a SqlConnection Object
+        public static SqlConnection GetConnection() {
+            SqlConnection conn = new SqlConnection(connectionString);
+            return conn;
+        }
+    }
+}
---
+++ VehicleServiceCenter/LogIN.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VehicleServiceCenter
+{
+    public partial class LogIN : Form
+    {
+        public LogIN()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
