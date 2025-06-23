22724a8754d9f20cd2ac508425c79cd3cc75eb0a
ArinoLane <arindom793@gmail.com>
2025-06-23 21:27:23 +0600

    admin

--- VehicleServiceCenter/AdminCustomerForm.Designer.cs
+++ VehicleServiceCenter/AdminCustomerForm.Designer.cs
@@ -43,26 +43,7 @@
             label6 = new Label();
             pictureBox20 = new PictureBox();
             PanelHome = new Panel();
-            label15 = new Label();
-            panel9 = new Panel();
-            label16 = new Label();
-            label13 = new Label();
-            panel8 = new Panel();
-            label14 = new Label();
-            label11 = new Label();
-            panel5 = new Panel();
-            label12 = new Label();
             label10 = new Label();
-            label9 = new Label();
-            label8 = new Label();
-            label5 = new Label();
-            panel7 = new Panel();
-            label4 = new Label();
-            panel6 = new Panel();
-            label3 = new Label();
-            label1 = new Label();
-            panel4 = new Panel();
-            label2 = new Label();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             PanelProfile.SuspendLayout();
@@ -278,133 +259,12 @@
             // PanelHome
             // 
             PanelHome.BackColor = Color.FromArgb(18, 18, 18);
-            PanelHome.Controls.Add(label15);
-            PanelHome.Controls.Add(panel9);
-            PanelHome.Controls.Add(label16);
-            PanelHome.Controls.Add(label13);
-            PanelHome.Controls.Add(panel8);
-            PanelHome.Controls.Add(label14);
-            PanelHome.Controls.Add(label11);
-            PanelHome.Controls.Add(panel5);
-            PanelHome.Controls.Add(label12);
             PanelHome.Controls.Add(label10);
-            PanelHome.Controls.Add(label9);
-            PanelHome.Controls.Add(label8);
-            PanelHome.Controls.Add(label5);
-            PanelHome.Controls.Add(panel7);
-            PanelHome.Controls.Add(label4);
-            PanelHome.Controls.Add(panel6);
-            PanelHome.Controls.Add(label3);
-            PanelHome.Controls.Add(label1);
-            PanelHome.Controls.Add(panel4);
-            PanelHome.Controls.Add(label2);
             PanelHome.Dock = DockStyle.Fill;
             PanelHome.Location = new Point(169, 55);
             PanelHome.Name = "PanelHome";
             PanelHome.Size = new Size(839, 482);
             PanelHome.TabIndex = 6;
-            // 
-            // label15
-            // 
-            label15.AutoSize = true;
-            label15.BackColor = Color.Transparent;
-            label15.FlatStyle = FlatStyle.Flat;
-            label15.Font = new Font("Poppins", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label15.ForeColor = Color.WhiteSmoke;
-            label15.Location = new Point(596, 307);
-            label15.Name = "label15";
-            label15.Size = new Size(51, 23);
-            label15.TabIndex = 20;
-            label15.Text = "ADMIN";
-            // 
-            // panel9
-            // 
-            panel9.BackColor = Color.White;
-            panel9.Location = new Point(587, 311);
-            panel9.Name = "panel9";
-            panel9.Size = new Size(3, 80);
-            panel9.TabIndex = 18;
-            // 
-            // label16
-            // 
-            label16.AutoSize = true;
-            label16.BackColor = Color.Transparent;
-            label16.FlatStyle = FlatStyle.Flat;
-            label16.Font = new Font("Poppins", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label16.ForeColor = Color.WhiteSmoke;
-            label16.Location = new Point(583, 311);
-            label16.Name = "label16";
-            label16.Size = new Size(130, 113);
-            label16.TabIndex = 19;
-            label16.Text = "00";
-            // 
-            // label13
-            // 
-            label13.AutoSize = true;
-            label13.BackColor = Color.Transparent;
-            label13.FlatStyle = FlatStyle.Flat;
-            label13.Font = new Font("Poppins", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label13.ForeColor = Color.WhiteSmoke;
-            label13.Location = new Point(326, 307);
-            label13.Name = "label13";
-            label13.Size = new Size(95, 23);
-            label13.TabIndex = 17;
-            label13.Text = "RECEPTIONIST";
-            // 
-            // panel8
-            // 
-            panel8.BackColor = Color.White;
-            panel8.Location = new Point(317, 311);
-            panel8.Name = "panel8";
-            panel8.Size = new Size(3, 80);
-            panel8.TabIndex = 15;
-            // 
-            // label14
-            // 
-            label14.AutoSize = true;
-            label14.BackColor = Color.Transparent;
-            label14.FlatStyle = FlatStyle.Flat;
-            label14.Font = new Font("Poppins", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label14.ForeColor = Color.WhiteSmoke;
-            label14.Location = new Point(313, 311);
-            label14.Name = "label14";
-            label14.Size = new Size(130, 113);
-            label14.TabIndex = 16;
-            label14.Text = "00";
-            // 
-            // label11
-            // 
-            label11.AutoSize = true;
-            label11.BackColor = Color.Transparent;
-            label11.FlatStyle = FlatStyle.Flat;
-            label11.Font = new Font("Poppins", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label11.ForeColor = Color.WhiteSmoke;
-            label11.Location = new Point(98, 307);
-            label11.Name = "label11";
-            label11.Size = new Size(78, 23);
-            label11.TabIndex = 14;
-            label11.Text = "MECHANIC";
-            // 
-            // panel5
-            // 
-            panel5.BackColor = Color.White;
-            panel5.Location = new Point(89, 311);
-            panel5.Name = "panel5";
-            panel5.Size = new Size(3, 80);
-            panel5.TabIndex = 12;
-            // 
-            // label12
-            // 
-            label12.AutoSize = true;
-            label12.BackColor = Color.Transparent;
-            label12.FlatStyle = FlatStyle.Flat;
-            label12.Font = new Font("Poppins", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label12.ForeColor = Color.WhiteSmoke;
-            label12.Location = new Point(85, 311);
-            label12.Name = "label12";
-            label12.Size = new Size(130, 113);
-            label12.TabIndex = 13;
-            label12.Text = "00";
             // 
             // label10
             // 
@@ -418,121 +278,6 @@
             label10.Size = new Size(295, 37);
             label10.TabIndex = 11;
             label10.Text = "CUSTOMER INFORMATION";
-            // 
-            // label9
-            // 
-            label9.AutoSize = true;
-            label9.BackColor = Color.Transparent;
-            label9.FlatStyle = FlatStyle.Flat;
-            label9.Font = new Font("Poppins", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label9.ForeColor = Color.WhiteSmoke;
-            label9.Location = new Point(597, 132);
-            label9.Name = "label9";
-            label9.Size = new Size(106, 23);
-            label9.TabIndex = 10;
-            label9.Text = "WORK PENDING";
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.BackColor = Color.Transparent;
-            label8.FlatStyle = FlatStyle.Flat;
-            label8.Font = new Font("Poppins", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label8.ForeColor = Color.WhiteSmoke;
-            label8.Location = new Point(331, 132);
-            label8.Name = "label8";
-            label8.Size = new Size(52, 23);
-            label8.TabIndex = 9;
-            label8.Text = "PROFIT";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.BackColor = Color.Transparent;
-            label5.FlatStyle = FlatStyle.Flat;
-            label5.Font = new Font("Poppins", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label5.ForeColor = Color.WhiteSmoke;
-            label5.Location = new Point(98, 128);
-            label5.Name = "label5";
-            label5.Size = new Size(79, 23);
-            label5.TabIndex = 8;
-            label5.Text = "CUSTOMER";
-            // 
-            // panel7
-            // 
-            panel7.BackColor = Color.White;
-            panel7.Location = new Point(588, 132);
-            panel7.Name = "panel7";
-            panel7.Size = new Size(3, 80);
-            panel7.TabIndex = 6;
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.BackColor = Color.Transparent;
-            label4.FlatStyle = FlatStyle.Flat;
-            label4.Font = new Font("Poppins", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.ForeColor = Color.Lime;
-            label4.Location = new Point(583, 132);
-            label4.Name = "label4";
-            label4.Size = new Size(146, 113);
-            label4.TabIndex = 7;
-            label4.Text = "NO";
-            // 
-            // panel6
-            // 
-            panel6.BackColor = Color.White;
-            panel6.Location = new Point(322, 132);
-            panel6.Name = "panel6";
-            panel6.Size = new Size(3, 80);
-            panel6.TabIndex = 4;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.BackColor = Color.Transparent;
-            label3.FlatStyle = FlatStyle.Flat;
-            label3.Font = new Font("Poppins", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.ForeColor = Color.WhiteSmoke;
-            label3.Location = new Point(313, 132);
-            label3.Name = "label3";
-            label3.Size = new Size(130, 113);
-            label3.TabIndex = 5;
-            label3.Text = "00";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.BackColor = Color.Transparent;
-            label1.FlatStyle = FlatStyle.Flat;
-            label1.Font = new Font("Poppins Medium", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.ForeColor = Color.WhiteSmoke;
-            label1.Location = new Point(73, 76);
-            label1.Name = "label1";
-            label1.Size = new Size(295, 37);
-            label1.TabIndex = 2;
-            label1.Text = "CUSTOMER INFORMATION";
-            // 
-            // panel4
-            // 
-            panel4.BackColor = Color.White;
-            panel4.Location = new Point(89, 132);
-            panel4.Name = "panel4";
-            panel4.Size = new Size(3, 80);
-            panel4.TabIndex = 0;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.BackColor = Color.Transparent;
-            label2.FlatStyle = FlatStyle.Flat;
-            label2.Font = new Font("Poppins", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.ForeColor = Color.WhiteSmoke;
-            label2.Location = new Point(85, 132);
-            label2.Name = "label2";
-            label2.Size = new Size(130, 113);
-            label2.TabIndex = 3;
-            label2.Text = "00";
             // 
             // AdminCustomerForm
             // 
@@ -571,25 +316,6 @@
         private Label label6;
         private PictureBox pictureBox20;
         private Panel PanelHome;
-        private Label label15;
-        private Panel panel9;
-        private Label label16;
-        private Label label13;
-        private Panel panel8;
-        private Label label14;
-        private Label label11;
-        private Panel panel5;
-        private Label label12;
         private Label label10;
-        private Label label9;
-        private Label label8;
-        private Label label5;
-        private Panel panel7;
-        private Label label4;
-        private Panel panel6;
-        private Label label3;
-        private Label label1;
-        private Panel panel4;
-        private Label label2;
     }
 }
