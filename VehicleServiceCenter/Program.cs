6a7a0b0bbf13fa87afaf9e2e34ec6960e4e9b7c5
ArinoLane <arindom793@gmail.com>
2025-06-26 22:00:59 +0600

    main

--- VehicleServiceCenter/Program.cs
+++ VehicleServiceCenter/Program.cs
@@ -7,62 +7,16 @@ namespace VehicleServiceCenter
         static void Main(string[] args)
         {
 
-            //string connString = DbConfig.connectionString;
-
-
-            //try {
-            //    using (SqlConnection conn = new SqlConnection(connString)) {
-            //        conn.Open();
-
-            //        string insertQuery = @"
-            //            INSERT INTO Users 
-            //            (Name, Gender, Password, DateOfBirth, BloodGroup, Email, UserType) 
-            //            VALUES (@Name, @Gender, @Password, @DateOfBirth, @BloodGroup, @Email, @UserType);
-            //        ";
-
-            //        using (SqlCommand cmd = new SqlCommand(insertQuery, conn)) {
-            //            // Set parameter values
-            //            cmd.Parameters.AddWithValue("@Name", "Tipto");
-            //            cmd.Parameters.AddWithValue("@Gender", "Male");
-            //            cmd.Parameters.AddWithValue("@Password", "1234"); 
-            //            cmd.Parameters.AddWithValue("@DateOfBirth", new DateTime(2004, 12, 5));
-            //            cmd.Parameters.AddWithValue("@BloodGroup", "B-");
-            //            cmd.Parameters.AddWithValue("@Email", "tipto@gmail.com");
-            //            cmd.Parameters.AddWithValue("@UserType", "Customer");
-
-            //            int rowsAffected = cmd.ExecuteNonQuery();
-
-            //            Console.WriteLine($"{rowsAffected} row(s) inserted successfully.");
-            //        }
-            //    }
-            //} catch (Exception ex) {
-            //    Console.WriteLine("Error: " + ex.Message);
-            //}
-
             UserRepository userRepository = new UserRepository();
-
-            //User user = userRepository.GetUserById(1000);
-            //User user = userRepository.GetUserByEmail("bob.manager@example.com");
-
-            //if (user == null) {
-            //    Console.WriteLine("Failed");
-            //}else {
-            //    Console.WriteLine(user.Name);
-            //}
-
-
-            //int check = userRepository.DeleteUser(1014); 
 
 
 
             AdminRepository adminRepository = new AdminRepository();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            // Application.Run(new Form1());
+ 
 
 
-
-            //arko form 
             //Application.Run(new LogIN());
             //Application.Run(new LoginForm());
             //Application.Run(new ShopForm());
@@ -76,28 +30,6 @@ namespace VehicleServiceCenter
             //Application.Run(new AdminCustomerForm());
             Application.Run(new ShopForm());
 
-            //practice
-            //arino's comment
-
-            //Console.WriteLine("jhaisd");
-
-
-            //UserService userService = new UserService();
-
-            //User user = userService.Login(1000, "pass123");
-
-            //if (user == null) {
-            //    Console.WriteLine("Log in failed");
-            //} else {
-            //    Console.WriteLine(user.Name);
-            //}
-
-
-            //Console.ReadLine();
-
-
-            // Tipto's Note
-            // Opening My new branch. Date: 9-th June-2025
         }
     }
 }
