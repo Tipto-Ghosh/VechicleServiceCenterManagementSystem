74c7d853c0018fe550344e6beac534ae4c9dda11
ReDThunDeR33 <mdarko190@gmail.com>
2025-06-27 17:24:20 +0600

    .

---
+++ VehicleServiceCenter/Service_Form.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace VehicleServiceCenter
+{
+    partial class Service_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            profilePanel = new Panel();
+            settingsPictureBox = new PictureBox();
+            notificationPictureBox = new PictureBox();
+            fullNameLabel = new Label();
+            signOutPictureBox = new PictureBox();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            UserNameTxtBox = new TextBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button3 = new Button();
+            button4 = new Button();
+            label5 = new Label();
+            textBox_UserID = new TextBox();
+            label_userID = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            profilePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)settingsPictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)notificationPictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)signOutPictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // profilePanel
+            // 
+            profilePanel.BackColor = Color.FromArgb(38, 38, 38);
+            profilePanel.Controls.Add(label5);
+            profilePanel.Controls.Add(settingsPictureBox);
+            profilePanel.Controls.Add(notificationPictureBox);
+            profilePanel.Controls.Add(fullNameLabel);
+            profilePanel.Controls.Add(signOutPictureBox);
+            profilePanel.Dock = DockStyle.Top;
+            profilePanel.Location = new Point(0, 0);
+            profilePanel.Margin = new Padding(0);
+            profilePanel.Name = "profilePanel";
+            profilePanel.Size = new Size(1152, 73);
+            profilePanel.TabIndex = 5;
+            // 
+            // settingsPictureBox
+            // 
+            settingsPictureBox.Image = Properties.Resources.car_maintenance_17073141;
+            settingsPictureBox.Location = new Point(12, 4);
+            settingsPictureBox.Margin = new Padding(3, 4, 3, 4);
+            settingsPictureBox.Name = "settingsPictureBox";
+            settingsPictureBox.Size = new Size(65, 73);
+            settingsPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            settingsPictureBox.TabIndex = 3;
+            settingsPictureBox.TabStop = false;
+            // 
+            // notificationPictureBox
+            // 
+            notificationPictureBox.Image = Properties.Resources.icons8_customer_64;
+            notificationPictureBox.Location = new Point(886, 0);
+            notificationPictureBox.Margin = new Padding(3, 4, 3, 4);
+            notificationPictureBox.Name = "notificationPictureBox";
+            notificationPictureBox.Size = new Size(35, 73);
+            notificationPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            notificationPictureBox.TabIndex = 3;
+            notificationPictureBox.TabStop = false;
+            // 
+            // fullNameLabel
+            // 
+            fullNameLabel.AutoSize = true;
+            fullNameLabel.BackColor = Color.Transparent;
+            fullNameLabel.Font = new Font("Poppins Medium", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            fullNameLabel.ForeColor = Color.WhiteSmoke;
+            fullNameLabel.Location = new Point(513, 18);
+            fullNameLabel.Name = "fullNameLabel";
+            fullNameLabel.Size = new Size(151, 36);
+            fullNameLabel.TabIndex = 2;
+            fullNameLabel.Text = "ADD SERVICE";
+            // 
+            // signOutPictureBox
+            // 
+            signOutPictureBox.Dock = DockStyle.Right;
+            signOutPictureBox.Image = Properties.Resources.icons8_sign_out_501;
+            signOutPictureBox.Location = new Point(1117, 0);
+            signOutPictureBox.Margin = new Padding(3, 4, 3, 4);
+            signOutPictureBox.Name = "signOutPictureBox";
+            signOutPictureBox.Size = new Size(35, 73);
+            signOutPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            signOutPictureBox.TabIndex = 1;
+            signOutPictureBox.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = Color.FromArgb(136, 136, 136);
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Left;
+            dataGridView1.Location = new Point(0, 73);
+            dataGridView1.Margin = new Padding(0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(663, 643);
+            dataGridView1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Cascadia Code", 10F);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(678, 206);
+            label1.Name = "label1";
+            label1.Size = new Size(130, 22);
+            label1.TabIndex = 31;
+            label1.Text = "SERVICE NAME";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Cascadia Code", 10F);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(678, 261);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 22);
+            label2.TabIndex = 32;
+            label2.Text = "PRICE";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Cascadia Code", 10F);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(678, 315);
+            label3.Name = "label3";
+            label3.Size = new Size(150, 22);
+            label3.TabIndex = 33;
+            label3.Text = "ESTIMETED TIME";
+            // 
+            // UserNameTxtBox
+            // 
+            UserNameTxtBox.BackColor = Color.FromArgb(124, 124, 124);
+            UserNameTxtBox.BorderStyle = BorderStyle.None;
+            UserNameTxtBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            UserNameTxtBox.ForeColor = SystemColors.InactiveBorder;
+            UserNameTxtBox.Location = new Point(834, 198);
+            UserNameTxtBox.Multiline = true;
+            UserNameTxtBox.Name = "UserNameTxtBox";
+            UserNameTxtBox.Size = new Size(295, 30);
+            UserNameTxtBox.TabIndex = 35;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(124, 124, 124);
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.ForeColor = SystemColors.InactiveBorder;
+            textBox1.Location = new Point(834, 253);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(295, 30);
+            textBox1.TabIndex = 36;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(124, 124, 124);
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.ForeColor = SystemColors.InactiveBorder;
+            textBox2.Location = new Point(834, 310);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(295, 30);
+            textBox2.TabIndex = 37;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ControlLightLight;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            button3.ForeColor = Color.Green;
+            button3.Location = new Point(746, 514);
+            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Name = "button3";
+            button3.Size = new Size(121, 48);
+            button3.TabIndex = 40;
+            button3.Text = "ADD";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ControlLightLight;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Popup;
+            button4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = Color.Red;
+            button4.Location = new Point(945, 514);
+            button4.Margin = new Padding(3, 4, 3, 4);
+            button4.Name = "button4";
+            button4.Size = new Size(121, 48);
+            button4.TabIndex = 41;
+            button4.Text = "CANCEL";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Poppins Medium", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.WhiteSmoke;
+            label5.Location = new Point(927, 18);
+            label5.Name = "label5";
+            label5.Size = new Size(125, 36);
+            label5.TabIndex = 4;
+            label5.Text = "FULL NAME";
+            label5.Click += label5_Click;
+            // 
+            // textBox_UserID
+            // 
+            textBox_UserID.BackColor = Color.FromArgb(124, 124, 124);
+            textBox_UserID.BorderStyle = BorderStyle.None;
+            textBox_UserID.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox_UserID.ForeColor = SystemColors.InactiveBorder;
+            textBox_UserID.Location = new Point(834, 145);
+            textBox_UserID.Multiline = true;
+            textBox_UserID.Name = "textBox_UserID";
+            textBox_UserID.ReadOnly = true;
+            textBox_UserID.Size = new Size(295, 30);
+            textBox_UserID.TabIndex = 34;
+            textBox_UserID.Text = "System Auto Generated";
+            // 
+            // label_userID
+            // 
+            label_userID.AutoSize = true;
+            label_userID.Font = new Font("Cascadia Code", 10F);
+            label_userID.ForeColor = Color.White;
+            label_userID.Location = new Point(678, 147);
+            label_userID.Name = "label_userID";
+            label_userID.Size = new Size(110, 22);
+            label_userID.TabIndex = 30;
+            label_userID.Text = "SERVICE ID";
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.FromArgb(124, 124, 124);
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.ForeColor = SystemColors.InactiveBorder;
+            textBox3.Location = new Point(834, 372);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(295, 91);
+            textBox3.TabIndex = 39;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Cascadia Code", 10F);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(678, 380);
+            label4.Name = "label4";
+            label4.Size = new Size(120, 22);
+            label4.TabIndex = 38;
+            label4.Text = "DESCRIPTION";
+            label4.Click += label4_Click;
+            // 
+            // Service_Form
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(18, 18, 18);
+            ClientSize = new Size(1152, 716);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(UserNameTxtBox);
+            Controls.Add(textBox_UserID);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(label_userID);
+            Controls.Add(dataGridView1);
+            Controls.Add(profilePanel);
+            Name = "Service_Form";
+            Text = "Service_Form";
+            profilePanel.ResumeLayout(false);
+            profilePanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)settingsPictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)notificationPictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)signOutPictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel profilePanel;
+        private PictureBox settingsPictureBox;
+        private PictureBox notificationPictureBox;
+        private Label fullNameLabel;
+        private PictureBox signOutPictureBox;
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox UserNameTxtBox;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button3;
+        private Button button4;
+        private Label label5;
+        private TextBox textBox_UserID;
+        private Label label_userID;
+        private TextBox textBox3;
+        private Label label4;
+    }
+}
