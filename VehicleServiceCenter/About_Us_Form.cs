8d4c9042939a8cabb7698df3f02241f4e1e441c7
ReDThunDeR33 <mdarko190@gmail.com>
2025-06-28 14:18:18 +0600

    about us done

---
+++ VehicleServiceCenter/About_Us_Form.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VehicleServiceCenter
+{
+    public partial class About_Us_Form : Form
+    {
+        public About_Us_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void PanelProfile_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
